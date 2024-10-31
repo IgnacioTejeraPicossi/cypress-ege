04893ad0a615423684f8f4b72e0e00472f64531f
IgnacioTejeraPicossi <ignacio.tejera@outlook.com>
2024-10-31 14:13:41 +0100

    Add URLs

--- cypress.config.js
+++ cypress.config.js
@@ -2,8 +2,11 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: 'http://www.ege.no',// Angi URL for lokal installasjon
-    //baseUrl: 'www.ege.no',// Gamle URL for in.yngve ege
+    baseUrl: 'http://www.ege.no',// Gamle URL for in.yngve ege
+    //baseUrl: 'http://ege-xp7prod.enonic.cloud/admin/site/preview/ege/draft/eger',// Angi URL for prod installasjon
+    //'http://localhost:8080/admin/site/preview/ege/draft/ege' // Angi URL for lokal installasjon
+    //'http://localhost:8080/site/default/master/no' //Angi URL for lokal installasjon
+        
    
     setupNodeEvents(on, config) {
       // implement node event listeners here
