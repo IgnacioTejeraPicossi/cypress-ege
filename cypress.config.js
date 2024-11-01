849e99f7e1c9961eb38947312aca1b9c3290b5fb
IgnacioTejeraPicossi <ignacio.tejera@outlook.com>
2024-11-01 15:14:03 +0100

    Add Content studio URLs

--- cypress.config.js
+++ cypress.config.js
@@ -6,6 +6,8 @@ module.exports = defineConfig({
     //baseUrl: 'http://ege-xp7prod.enonic.cloud/admin/site/preview/ege/draft/eger',// Angi URL for prod installasjon
     //'http://localhost:8080/admin/site/preview/ege/draft/ege' // Angi URL for lokal installasjon
     //'http://localhost:8080/site/default/master/no' //Angi URL for lokal installasjon
+    //http://localhost:8080/admin/tool/com.enonic.app.contentstudio/main#/ege/browse //Content Studio URL
+    
         
    
     setupNodeEvents(on, config) {
