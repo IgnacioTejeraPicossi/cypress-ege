3e948a852e12e4e841c2b11bfc0145fdd382b31c
IgnacioTejeraPicossi <ignacio.tejera@outlook.com>
2024-11-13 12:31:07 +0100

    Addmore visores test

--- cypress/e2e/visores.cy.js
+++ cypress/e2e/visores.cy.js
@@ -35,4 +35,28 @@ describe('Responsivitetstester', () => {
       cy.get('nav').should('be.visible');
     });
 
+    it('Test på Google Pixel 5, Legger til tester for en populær Android-enhet med standardoppløsning', () => {
+      cy.viewport(1080, 2340); // Oppløsning for Pixel 5
+      cy.visit('/');
+      cy.get('nav').should('be.visible');
+  });
+
+  it('Test på Surface Duo - Enheten har et unikt skjermforhold, nyttig for å teste oppdeling og tilpasning', () => {
+      cy.viewport(1350, 1800); // Oppløsning for Surface Duo
+      cy.visit('/');
+      cy.get('nav').should('be.visible');
+  });
+
+  it('Test på stor skrivebordsoppløsning (1920x1080), dekker en større skjermstandard for skrivebordsbruk.', () => {
+      cy.viewport(1920, 1080); // Full HD skrivebordsoppløsning
+      cy.visit('/');
+      cy.get('nav').should('be.visible');
+  });
+
+  it('Test på ultrabred skjerm (2560x1440), for å teste oppsett på store skjermer eller monitorer med høy oppløsning.', () => {
+      cy.viewport(2560, 1440); // WQHD oppløsning
+      cy.visit('/');
+      cy.get('nav').should('be.visible');
+  });
+
   });
