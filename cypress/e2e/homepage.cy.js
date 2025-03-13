387e5ad981008de876bba72daef5ff4d0b954cdb
IgnacioTejeraPicossi <ignacio.tejera@outlook.com>
2025-03-13 15:03:45 +0100

    Fix new menu

--- cypress/e2e/homepage.cy.js
+++ cypress/e2e/homepage.cy.js
@@ -6,7 +6,7 @@ describe('Hjemmeside norsk test', () => {
       //cy.visit('http://localhost:8080/admin/site/preview/starter/draft/ege/');
 
 
-      cy.contains('Din lokale leverandør').should('be.visible');  // Sjekk at teksten "Din lokale leverandør" er synlig
+      cy.contains('Din partner for økt produksjonseffektivitet').should('be.visible');  // Sjekk at teksten "Din lokale leverandør" er synlig
       
     });
   });
